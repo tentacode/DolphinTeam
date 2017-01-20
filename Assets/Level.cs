47eb03844b3ea0a409ae3ea9123016bd1c5edff6
GFX47 <dubois.sebastien@gmail.com>
2017-01-20 23:27:30 +0100

    First prototype! \o/

---
+++ Assets/Level.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+	[SerializeField]
+	private float speed;
+
+	private Transform tfm;
+
+	private void Awake()
+	{
+		this.tfm = this.GetComponent<Transform>();
+	}
+
+	private void Update()
+	{
+		this.tfm.position += this.speed * Time.deltaTime * Vector3.up;
+	}
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	[SerializeField]
+	private float moveUnit;
+
+	private Transform tfm;
+	private int hPosition = 0;
+
+	private void Awake()
+	{
+		this.tfm = this.GetComponent<Transform>();
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		Debug.Log(other.name);
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyUp(KeyCode.LeftArrow))
+		{
+			--this.hPosition;
+		}
+		else if (Input.GetKeyUp(KeyCode.RightArrow))
+		{
+			++this.hPosition;
+		}
+
+		this.hPosition = Mathf.Clamp(this.hPosition, -1, 1);
+
+		this.tfm.localPosition = new Vector3
+		(
+			(float)this.hPosition * this.moveUnit,
+			this.tfm.localPosition.y,
+			this.tfm.localPosition.z
+		);
+	}
+}
