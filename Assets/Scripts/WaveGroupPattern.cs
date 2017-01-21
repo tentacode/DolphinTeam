7f9974bfea20410ab12c953e8cbae7a7b887d1f3
GFX47 <dubois.sebastien@gmail.com>
2017-01-21 21:46:14 +0100

    Improved game objects naming

--- Assets/Scripts/WaveGroupPattern.cs
+++ Assets/Scripts/WaveGroupPattern.cs
@@ -15,10 +15,6 @@ public class WaveGroupPattern : ScriptableObject
 		{
 			// Generate wave
 			WavePattern wavePattern = this.WavePatterns[wavePatternIndex];
-
-			GameObject waveGO = GameObject.Instantiate(gameConfig.WavePrefab, waveYPosition * Vector3.up + gameConfig.WavePrefab.transform.position.z * Vector3.forward, Quaternion.identity);
-			waveGO.name = wavePattern.name;
-
 			wavePattern.SpawnHazards(gameConfig, playerCount, waveYPosition);
 
 			waveYPosition += waveSize;
