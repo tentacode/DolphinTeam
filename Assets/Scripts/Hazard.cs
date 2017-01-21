1960abc32c4cae4a0e17bf733ebbcdbc31598e00
GFX47 <dubois.sebastien@gmail.com>
2017-01-21 21:16:37 +0100

    Implemented chapters and transitions

--- Assets/Scripts/Hazard.cs
+++ Assets/Scripts/Hazard.cs
@@ -27,12 +27,12 @@ public class Hazard : AdvancedMonoBehaviour
 		AirTreasure,
 	}
 
-	public void Init(HazardType type, int colorIndex, bool noHazardHiding)
+	public void Init(HazardType type, int playerIndex, bool noHazardHiding)
 	{
 		this.Type = type;
         this.Collider = GetComponent<BoxCollider2D>();
 
-        if (!noHazardHiding && Game.Instance.LocalPlayerIndex == colorIndex)
+        if (!noHazardHiding && Game.Instance.LocalPlayerIndex == playerIndex)
 		{
 			// Hidden
 			this.gameObject.layer = LayerMask.NameToLayer("HiddenHazard");
@@ -41,10 +41,10 @@ public class Hazard : AdvancedMonoBehaviour
 		this.spriteRenderer.sprite = this.gameConfig.GetHazardSprite(this.Type);
         this.audioSource.clip = this.gameConfig.GetHazardAudio(this.Type);
         this.animator.runtimeAnimatorController = this.gameConfig.GetHazardAnimator(this.Type);
-		//this.spriteRenderer.color = noHazardHiding ? this.gameConfig.NeutralColor : this.gameConfig.PlayerColors[colorIndex];
+		//this.spriteRenderer.color = noHazardHiding ? this.gameConfig.NeutralColor : this.gameConfig.PlayerColors[playerIndex];
 	}
 
-    public void OnPlayerDeadlyCollision()
+	public void OnPlayerDeadlyCollision()
     {
 		if (this.animator.runtimeAnimatorController == null)
 		{
--- Assets/Scripts/WaveGroupPattern.cs
+++ Assets/Scripts/WaveGroupPattern.cs
@@ -5,4 +5,22 @@ public class WaveGroupPattern : ScriptableObject
 {
 	public int Difficulty;
 	public WavePattern[] WavePatterns;
+
+	public void SpawnWaves(GameConfig gameConfig, int playerCount, float waveSize, ref float waveYPosition)
+	{
+		//Debug.LogFormat("WaveGroup > {0}",  difficultyWaveGroupPattern.name);
+
+		for (int wavePatternIndex = 0; wavePatternIndex < this.WavePatterns.Length; ++wavePatternIndex)
+		{
+			// Generate wave
+			WavePattern wavePattern = this.WavePatterns[wavePatternIndex];
+
+			GameObject waveGO = GameObject.Instantiate(gameConfig.WavePrefab, waveYPosition * Vector3.up + gameConfig.WavePrefab.transform.position.z * Vector3.forward, Quaternion.identity);
+			waveGO.name = wavePattern.name;
+
+			wavePattern.SpawnHazards(gameConfig, playerCount, waveYPosition);
+
+			waveYPosition += waveSize;
+		}
+	}
 }
