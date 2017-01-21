04371e4d5f173e60318c1bf8df95b0b193b2f423
Laurent Victorino <lvictorino@monkeymoon.net>
2017-01-21 14:48:36 +0100

    UI: Treasure counter.

---
+++ Assets/Scripts/UI/TreasureDisplay.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TreasureDisplay : MonoBehaviour
+{
+    [SerializeField]
+    private Text text;
+
+    public void UpdateDisplayCount(int treasureCount)
+    {
+        text.text = treasureCount.ToString();
+    }
+}
