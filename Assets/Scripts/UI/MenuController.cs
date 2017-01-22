559fd72f8db574a4475700e9fd480a582851f234
tentacode <contact@gabrielpillet.com>
2017-01-22 09:49:47 +0100

    Input seed

--- Assets/Scripts/UI/MenuController.cs
+++ Assets/Scripts/UI/MenuController.cs
@@ -7,10 +7,14 @@ public class MenuController : MonoBehaviour
 	public List<GameObject> panels;
 	public GameObject initialPanel;
 	public List<Image> masterSeedSymbols;
+	public List<Image> slaveSeedSymbols;
+	public Image playerSymbol;
 	public List<Sprite> playerSymbolSprites;
+	public List<Sprite> playerCountSprites;
 
 	private int playerNumber;
 	private int randomSeed;
+	private int playerCount = 2;
 
 	void Start()
 	{
@@ -93,5 +97,112 @@ public class MenuController : MonoBehaviour
 	public void StartGame()
 	{
 		HideEverythingBut("🤔");
+	}
+
+	public void SymbolInputUp(int symbolIndex)
+	{
+		Image inputSymbol = slaveSeedSymbols[symbolIndex];
+		Sprite sprite = inputSymbol.sprite;
+
+		switch (sprite.name) {
+			case "Symbols_0":
+				inputSymbol.sprite = playerSymbolSprites[1];
+				break;
+			case "Symbols_1":
+				inputSymbol.sprite = playerSymbolSprites[2];
+				break;
+			case "Symbols_2":
+				inputSymbol.sprite = playerSymbolSprites[3];
+				break;
+			default:
+				inputSymbol.sprite = playerSymbolSprites[0];
+				break;
+		}
+	}
+
+	public void SymbolInputDown(int symbolIndex)
+	{
+		Image inputSymbol = slaveSeedSymbols[symbolIndex];
+		Sprite sprite = inputSymbol.sprite;
+
+		switch (sprite.name) {
+			case "Symbols_3":
+				inputSymbol.sprite = playerSymbolSprites[2];
+				break;
+			case "Symbols_2":
+				inputSymbol.sprite = playerSymbolSprites[1];
+				break;
+			case "Symbols_1":
+				inputSymbol.sprite = playerSymbolSprites[0];
+				break;
+			default:
+				inputSymbol.sprite = playerSymbolSprites[3];
+				break;
+		}
+	}
+
+	public void PlayerSymbolUp()
+	{
+		Sprite sprite = playerSymbol.sprite;
+
+		switch (sprite.name) {
+			case "PlayerCount_0":
+				if (playerCount == 2) {
+					return;
+				}
+
+				playerSymbol.sprite = playerCountSprites[1];
+				break;
+			case "PlayerCount_1":
+				if (playerCount <= 3) {
+					playerSymbol.sprite = playerCountSprites[0];
+				}
+
+				playerSymbol.sprite = playerCountSprites[2];
+				break;
+			case "PlayerCount_2":
+				if (playerCount <= 4) {
+					playerSymbol.sprite = playerCountSprites[0];
+				}
+
+				playerSymbol.sprite = playerCountSprites[3];
+				break;
+			case "PlayerCount_3":
+				playerSymbol.sprite = playerCountSprites[0];
+				break;
+		}
+	}
+
+	public void PlayerSymbolDown()
+	{
+		Sprite sprite = playerSymbol.sprite;
+
+		switch (sprite.name) {
+			case "PlayerCount_0":
+				switch (playerCount) {
+					case 5:
+						playerSymbol.sprite = playerCountSprites[3];
+						break;
+					case 4:
+						playerSymbol.sprite = playerCountSprites[2];
+						break;
+					case 3:
+						playerSymbol.sprite = playerCountSprites[1];
+						break;
+					default:
+						return;
+				}
+
+				break;
+			case "PlayerCount_1":
+				playerSymbol.sprite = playerCountSprites[0];
+				break;
+			case "PlayerCount_2":
+				playerSymbol.sprite = playerCountSprites[1];
+				break;
+			case "PlayerCount_3":
+				playerSymbol.sprite = playerCountSprites[2];
+				break;
+		}
 	}
 }
